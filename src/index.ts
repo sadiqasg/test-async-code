ad5f54a841c103306f0ae594716a21b32ae6c05b
saslamp <sadiqasg@gmail.com>
2022-03-02 22:13:41 +0100

    ts node environment with eslint, prettier, and jasmine

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const myApp = (num:number) => num * num;
+
+export default myApp;
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,5 @@
+import myApp from "../index";
+
+it("expect myFunc(5) to equal 25", () => {
+    expect(myApp(5)).toEqual(25);
+});
